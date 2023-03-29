9cdb811fae6617e482b275e42edfcca2f7f37ef7
Alonso Cortes <alocortes.dev@gmail.com>
2023-03-28 18:37:08 -0600

    push to dev register component with validations. Added new global styles.

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -5,6 +5,8 @@ import { TranslateService } from '@ngx-translate/core';
 
 import { EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from 'app/config/error.constants';
 import { RegisterService } from './register.service';
+// @ts-ignore
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'jhi-register',
@@ -153,7 +155,15 @@ export class RegisterComponent implements AfterViewInit {
   }
 
   onUpload() {
-    if (!this.files[0]) alert('Debés primero arrastrar o seleccionar una imagen');
+    if (!this.files[0])
+      Swal.fire({
+        title: 'Error',
+        text: 'Debés primero arrastrar o seleccionar una imagen.',
+        type: 'error',
+        icon: 'error',
+        confirmButtonColor: '#3381f6',
+        confirmButtonText: 'Cerrar',
+      });
 
     const file_data = this.files[0];
     const data = new FormData();
@@ -165,12 +175,28 @@ export class RegisterComponent implements AfterViewInit {
       if (response) {
         const secureUrl = response.secure_url;
         this.photo.setValue(secureUrl);
-        console.log(this.photo.value);
+        Swal.fire({
+          title: 'Fotografía agregada',
+          text: 'Continuá registrando tus datos.',
+          type: 'success',
+          icon: 'success',
+          confirmButtonColor: '#3381f6',
+          confirmButtonText: 'Cerrar',
+        });
       }
     });
   }
 
   photo: FormControl = new FormControl('');
+  firstName: any;
+  firstLastName: any;
+  district: any;
+  canton: any;
+  province: any;
+  address: any;
+  phoneNumber: any;
+  identityNumber: any;
+  secondLastName: any;
 
   register(): void {
     this.doNotMatch = false;
@@ -180,7 +206,6 @@ export class RegisterComponent implements AfterViewInit {
 
     const photoValue = this.photo.value;
     const { password, confirmPassword } = this.registerForm.getRawValue();
-    // const { firstName, secondName, firstLastName, secondLastName, phoneNumber, identityNumber, address } = this.registerForm.controls;
     const { firstName, secondName, firstLastName, secondLastName, phoneNumber, identityNumber, address, province, canton, district } =
       this.registerForm.controls;
     if (password !== confirmPassword) {
@@ -205,16 +230,53 @@ export class RegisterComponent implements AfterViewInit {
           canton: canton.value,
           district: district.value,
         })
-        .subscribe({ next: () => (this.success = true), error: response => this.processError(response) });
+        .subscribe({
+          next: () => (
+            Swal.fire({
+              title: 'Registro exitoso',
+              text: 'Ya sos parte de FurryMatch',
+              type: 'success',
+              icon: 'success',
+              confirmButtonColor: '#3381f6',
+              confirmButtonText: 'Cerrar',
+            }),
+            (this.success = true)
+          ),
+          error: response => this.processError(response),
+        });
     }
   }
 
   private processError(response: HttpErrorResponse): void {
     if (response.status === 400 && response.error.type === LOGIN_ALREADY_USED_TYPE) {
+      Swal.fire({
+        title: 'Error',
+        text: 'El nombre de usuario ya existe. Por favor elegí otro.',
+        type: 'error',
+        icon: 'error',
+        confirmButtonColor: '#3381f6',
+        confirmButtonText: 'Cerrar',
+      });
       this.errorUserExists = true;
     } else if (response.status === 400 && response.error.type === EMAIL_ALREADY_USED_TYPE) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Correo electrónico ya registrado. Por favor intentá con otro.',
+        type: 'error',
+        icon: 'error',
+        confirmButtonColor: '#3381f6',
+        confirmButtonText: 'Cerrar',
+      });
       this.errorEmailExists = true;
     } else {
+      Swal.fire({
+        title: 'Error',
+        text: 'Registro fallido, intentalo más tarde.',
+        type: 'error',
+        icon: 'error',
+        confirmButtonColor: '#3381f6',
+        confirmButtonText: 'Cerrar',
+      });
       this.error = true;
     }
   }
