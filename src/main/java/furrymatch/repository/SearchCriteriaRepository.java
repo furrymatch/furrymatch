384af6fa088905bbec15f13b77ca0da3db1e7b15
Alina Zeledón <mzeledono@ucenfotec.ac.cr>
2023-04-17 20:54:00 -0600

    Fixed search criteria

--- src/main/java/furrymatch/repository/SearchCriteriaRepository.java
+++ src/main/java/furrymatch/repository/SearchCriteriaRepository.java
@@ -1,8 +1,7 @@
 package furrymatch.repository;
 
-import furrymatch.domain.Pet;
 import furrymatch.domain.SearchCriteria;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +14,7 @@ import org.springframework.stereotype.Repository;
 public interface SearchCriteriaRepository extends JpaRepository<SearchCriteria, Long> {
     @Query(value = "SELECT * FROM search_criteria WHERE pet_id = :id", nativeQuery = true)
     SearchCriteria findByPetId(@Param("id") Long id);
+
+    @Query(value = "SELECT * FROM search_criteria WHERE pet_id = :id", nativeQuery = true)
+    Optional<SearchCriteria> findByOwnerUser(@Param("id") Long id);
 }
